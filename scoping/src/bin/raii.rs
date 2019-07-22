2945deaa98abc29f5f76d30251e9338e6bd9b1b9
sabahtalateh <sabahtalateh@gmail.com>
2019-07-22 15:05:25 +0300

    WIP

--- scoping/src/bin/raii.rs
+++ scoping/src/bin/raii.rs
@@ -2,4 +2,21 @@ fn create_box() {
     let _box1 = Box::new(3i32);
 }
 
-fn main() {}
+fn main() {
+    // Allocate an integer on the heap
+    let _box2 = Box::new(5i32);
+
+    // A nested scope:
+    {
+        // Allocate an integer on the heap
+        let _box3 = Box::new(4i32);
+
+        // `_box3` is destroyed here, and memory gets freed
+    }
+
+    // Creating lots of boxes just for fun
+    // There's no need to manually free memory!
+    for _ in 0u32..1_000 {
+        create_box();
+    }
+}
